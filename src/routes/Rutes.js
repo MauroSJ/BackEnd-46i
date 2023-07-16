30f94a8afec62c9250757ebb88d2fc07a1366530
Mauro <mauro10z@hotmail.com>
2023-07-16 20:38:26 -0300

    Metodos Post, put y delete

--- src/routes/Rutes.js
+++ src/routes/Rutes.js
@@ -27,7 +27,40 @@ router.get('/canchas/:id', (req,res)=>{
 })
 
 
-//
+//POST
+router.post('/canchas', (req,res)=>{
+  const cancha = req.body
+  canchas.push(cancha)
+  res.status(201).json("Cancha creada")
+})
 
+
+//PUT   
+router.put('/canchas/:id', (req,res)=>{ 
+  const id = parseInt(req.params.id) 
+  const cancha = canchas.find((cancha)=> cancha.id == id) //busca la cancha
+  if (cancha){
+    cancha.nombre = req.body.nombre
+    cancha.capacidad = req.body.capacidad
+    cancha.direccion = req.body.direccion
+    res.status(200).json("Cancha modificada")
+    res.json(cancha)
+  } else {
+    res.status(404).json({error: "Cancha no encontrada"})
+  }
+
+})
+
+//DELETE
+router.delete('/canchas/:id', (req,res)=>{
+  const id = parseInt(req.params.id)
+  const index = canchas.findIndex((cancha)=>cancha.id == id)
+  if(index != -1){
+    canchas.splice(index, 1)
+    res.status(200).json("cancha eliminada")
+  } else {
+    res.status(404).json("Cancha no encontrada")
+  }
+})
 
 module.exports = router
